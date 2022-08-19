f017e06517b8b5135ac2b1af576d092152731b86
moizali-dev <moiz790@gmail.com>
2022-08-19 09:16:15 -0400

    Lecture 10 completed

---
+++ part1/10-Custom exceptions/exercise.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from email import message
+from pydantic import BaseModel, validator
+from typing import List, Union
+
+class TooFewDigitsError(Exception):
+
+    def __init__(self, value : int, message: str) -> None:
+        self.value = value
+        self.message = message
+
+class FirstNameNotInWorkEmailError(Exception):
+
+    def __init__(self, value : str, message: str) -> None:
+        self.value = value
+        self.message = message
+
+class Employee(BaseModel):
+
+    first_name:str
+    last_name:str
+    work_email:str
+    mobile_number:int
+    managers:Union[List[Employee], List[str]]
+    complete_name:str = None
+
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.complete_name = self.first_name + " " + self.last_name
+
+    @validator("mobile_number")
+    @classmethod
+    def check_mobile_number(cls, value:int) -> int:
+        if len(str(value)) < 6:
+            message = f"'{value}' is less than 6 digits"
+            raise TooFewDigitsError(value,message)
+
+        return value
+
+    @validator("work_email")
+    @classmethod
+    def check_work_email(cls, value:str, values: Dict[str, Any]) -> str:
+        if values["first_name"].lower() not in value:
+            msg = f"Work email passed '{value}' doesn't contain first_name: " \
+                  f"{values['first_name']}"
+            raise FirstNameNotInWorkEmailError(value, msg)
+        return value
+
+if __name__ == "__main__":
+    args_dict = {
+        "first_name":"Maryam",
+        "last_name":"Mirzakhani",
+        "work_email":"maryam.mirzakhani@awesome.com",
+        "mobile_number":"222333444",
+        "managers": ["Curtis"]
+    }
+    employee1 = Employee(**args_dict)
+    print(employee1)
